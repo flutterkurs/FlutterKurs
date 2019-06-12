9f8c48b446f9a4f3df21866e49e1e7918b27d4b1
Adam Włosek <a.wlosek@cctechnology.pl>
2019-06-12 15:33:45 +0200

    Dodano widget daty

--- todo_app/lib/task/NewTaskPage.dart
+++ todo_app/lib/task/NewTaskPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTaskPage extends StatefulWidget {
   NewTaskPage({Key key}) : super(key: key);
@@ -8,8 +9,8 @@ class NewTaskPage extends StatefulWidget {
 }
 
 class _NewTaskPageState extends State<NewTaskPage> {
-
   String _radioValue = 'low';
+  TextEditingController _dateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +19,35 @@ class _NewTaskPageState extends State<NewTaskPage> {
           title: Text('New Task'),
         ),
         body: Container(
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.all(15),
-          decoration: ShapeDecoration(
-              color: Colors.indigo[100],
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[_title(), _description(), _priority()],
-          ),
-        ));
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(15),
+            decoration: ShapeDecoration(
+                color: Colors.indigo[100],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)))),
+            child: ListView(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _title(),
+                    emptySpaceVertical(10),
+                    _description(),
+                    emptySpaceVertical(10),
+                    _date(),
+                    emptySpaceVertical(10),
+                    _priority()
+                  ],
+                ),
+              ],
+            )));
+  }
+
+  @override
+  void initState() {
+    DateTime now = DateTime.now();
+    _dateController.text = DateFormat('yyyy-MM-dd').format(now);
   }
 
   Widget _title() {
@@ -41,6 +59,12 @@ class _NewTaskPageState extends State<NewTaskPage> {
           icon: Icon(Icons.title),
           hintText: 'Enter title here'),
       autofocus: true,
+    );
+  }
+
+  Widget emptySpaceVertical(double space) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: space),
     );
   }
 
@@ -56,37 +80,85 @@ class _NewTaskPageState extends State<NewTaskPage> {
     );
   }
 
+  Widget _circle(String text) {
+    return Container(
+        width: 50,
+        height: 50,
+        decoration:
+            ShapeDecoration(color: Colors.deepPurple, shape: CircleBorder()),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.deepPurple[50]),
+        ));
+  }
+
+  Widget _divider(double width, double height, double margin) {
+    return Container(
+      width: width,
+      height: height,
+      margin: EdgeInsets.symmetric(horizontal: margin),
+      color: Colors.black,
+    );
+  }
+
+  Widget _date() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(children: <Widget>[
+        Icon(
+          Icons.date_range,
+          size: 25,
+          color: Colors.grey[700],
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+        ),
+        _circle('15'),
+        _divider(1, 50, 4),
+        _circle('05'),
+        _divider(1, 50, 4),
+        _circle('1994'),
+        IconButton(
+          icon: Icon(Icons.edit),
+          onPressed: () => {},
+        ),
+      ]),
+    );
+  }
+
   Widget _priority() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Text('Priority:', style: TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold)),
+        Text('Priority:',
+            style: TextStyle(
+                color: Colors.deepPurple, fontWeight: FontWeight.bold)),
         Radio<String>(
           value: 'low',
           groupValue: _radioValue,
           activeColor: Colors.green[400],
-          onChanged:(s)=> _handleRadioValueChange(s),
+          onChanged: (s) => _handleRadioValueChange(s),
         ),
         Radio<String>(
           groupValue: _radioValue,
           activeColor: Colors.yellow[400],
-          onChanged:(s)=> _handleRadioValueChange(s),
+          onChanged: (s) => _handleRadioValueChange(s),
           value: 'medium',
         ),
         Radio<String>(
           groupValue: _radioValue,
           activeColor: Colors.red[400],
-          onChanged:(s)=> _handleRadioValueChange(s),
+          onChanged: (s) => _handleRadioValueChange(s),
           value: 'high',
         ),
       ],
     );
   }
-  void _handleRadioValueChange(String value){
+
+  void _handleRadioValueChange(String value) {
     setState(() {
       _radioValue = value;
     });
-
   }
-
 }
